682bcc9208614c477b32b0e9c0af50a76ad8ef2a
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-10 08:19:24 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery")
+
+const links = document.querySelector(".gallery__item")
+
+function writeMarkup(arr) {
+   return arr.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `).join('')
+}
+
+ list.innerHTML = writeMarkup(galleryItems)
+
+
+console.log(list)
+console.log(links)
