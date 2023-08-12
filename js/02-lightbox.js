bc9b67f870700b54721a425d7c01d506523b79b4
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2023-08-12 08:28:45 +0200

    Done HW7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const list = document.querySelector(".gallery")
+
+function writeMarkup(arr) {
+   return arr.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li> `).join('')
+}
+
+
+list.insertAdjacentHTML("afterbegin", writeMarkup(galleryItems))
+
+list.addEventListener("click", getBigImg)
+function getBigImg(evt) {
+
+  evt.preventDefault()
+console.log(evt.target.alt)
+   if (!evt.target.classList.contains('gallery__image') ) {
+     return;
+   }
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+  });
+  
+}
+
